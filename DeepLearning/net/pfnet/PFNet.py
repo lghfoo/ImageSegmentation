30e22a5abbccf13fd7cbb5cb1ed086ffda2daea8
lghfoo <1162820772@qq.com>
2020-04-04 07:21:39 +0800

    remove conv

--- DeepLearning/net/pfnet/PFNet.py
+++ DeepLearning/net/pfnet/PFNet.py
@@ -16,26 +16,26 @@ class PFNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=100)    
         self.relu1 = nn.ReLU()                                                                                  
         # 第二层                                                                                    
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)             
-        self.relu2 = nn.ReLU()                                              
+        # self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)             
+        # self.relu2 = nn.ReLU()                                              
         # 第三层
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7, stride=2, padding=1)
         self.relu3 = nn.ReLU()
-        # 第四层
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.relu4 = nn.ReLU()
-        # 第五层
-        self.conv5 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.relu5 = nn.ReLU()
+        # # 第四层
+        # self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        # self.relu4 = nn.ReLU()
+        # # 第五层
+        # self.conv5 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        # self.relu5 = nn.ReLU()
         # 第六层
         self.conv6 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=7, stride=2, padding=1)
         self.relu6 = nn.ReLU()
-        # 第七层
-        self.conv7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.relu7 = nn.ReLU()
-        # 第八层
-        self.conv8 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.relu8 = nn.ReLU()
+        # # 第七层
+        # self.conv7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+        # self.relu7 = nn.ReLU()
+        # # 第八层
+        # self.conv8 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+        # self.relu8 = nn.ReLU()
         # 第九层
         self.conv9 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=7, stride=2, padding=1)
         self.relu9 = nn.ReLU()
@@ -54,23 +54,23 @@ class PFNet(nn.Module):
         # c1
         x = self.relu1(self.conv1(x))
         # c2
-        x = self.relu2(self.conv2(x)) # 1/2
+        # x = self.relu2(self.conv2(x)) # 1/2
         # c3
         x1 = self.relu3(self.conv3(x))
         size1 = x1.size()[2:]
 
         # c4
-        x = self.relu4(self.conv4(x1)) # 1/4
+        # x = self.relu4(self.conv4(x1)) # 1/4
         # c5
-        x = self.relu5(self.conv5(x))
+        # x = self.relu5(self.conv5(x))
         # c6
         x2 = self.relu6(self.conv6(x))
         size2 = x2.size()[2:]
 
         # c7
-        x = self.relu7(self.conv7(x2)) # 1/8
+        # x = self.relu7(self.conv7(x2)) # 1/8
         # c8
-        x = self.relu8(self.conv8(x))
+        # x = self.relu8(self.conv8(x))
         # c9
         x3 = self.relu9(self.conv9(x))
         size3 = x3.size()[2:]
