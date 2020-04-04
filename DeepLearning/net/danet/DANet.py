5a7de0cadf3639b103fb17da169367b7f17f43a7
lghfoo <1162820772@qq.com>
2020-04-04 16:42:50 +0800

    remove comment

--- DeepLearning/net/danet/DANet.py
+++ DeepLearning/net/danet/DANet.py
@@ -15,6 +15,7 @@ from torch.nn.parallel.parallel_apply import parallel_apply
 from torch.nn.parallel.scatter_gather import scatter
 import torchvision
 from torchvision.models import resnet
+
 class PAM_Module(nn.Module):
     """ Position attention module"""
     #Ref from SAGAN
@@ -49,7 +50,6 @@ class PAM_Module(nn.Module):
         out = self.gamma*out + x
         return out
 
-
 class CAM_Module(nn.Module):
     """ Channel attention module"""
     def __init__(self, in_dim):
@@ -81,119 +81,24 @@ class CAM_Module(nn.Module):
         out = self.gamma*out + x
         return out
 
-class BaseNet(nn.Module):
-    def __init__(self, nclass, aux, se_loss, dilated=True, norm_layer=None,
-                 base_size=576, crop_size=608, mean=[.485, .456, .406],
-                 std=[.229, .224, .225], root='./pretrain_models',
-                 multi_grid=False, multi_dilation=None):
-        super(BaseNet, self).__init__()
-        self.nclass = nclass
-        self.aux = aux
-        self.se_loss = se_loss
-        self.mean = mean
-        self.std = std
-        self.base_size = base_size
-        self.crop_size = crop_size
-        # copying modules from pretrained models
-        # self.pretrained = torchvision.models.resnet50(pretrained=False, replace_stride_with_dilation=[False, True, True])
-        # self.pretrained = torchvision.models.resnet101(pretrained=False)
-
-    def base_forward(self, x):
-        # x = self.pretrained.conv1(x)
-        # x = self.pretrained.bn1(x)
-        # x = self.pretrained.relu(x)
-        # x = self.pretrained.maxpool(x)
-        # c1 = self.pretrained.layer1(x)
-        # c2 = self.pretrained.layer2(c1)
-        # c3 = self.pretrained.layer3(c2)
-        # c4 = self.pretrained.layer4(c3)
-        # return c1, c2, c3, c4
-        return self.pretrained.layer4(self.pretrained.layer3(self.pretrained.layer2(self.pretrained.layer1(self.pretrained.relu(self.pretrained.bn1(self.pretrained.conv1(x)))))))
-
-class DANet(BaseNet):
-    r"""Fully Convolutional Networks for Semantic Segmentation
-
-    Parameters
-    ----------
-    nclass : int
-        Number of categories for the training dataset.
-    norm_layer : object
-        Normalization layer used in backbone network (default: :class:`mxnet.gluon.nn.BatchNorm`;
-
-
-    Reference:
-
-        Long, Jonathan, Evan Shelhamer, and Trevor Darrell. "Fully convolutional networks
-        for semantic segmentation." *CVPR*, 2015
-
-    """
-    def __init__(self, nclass, aux=False, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
-        super(DANet, self).__init__(nclass, aux, se_loss, norm_layer=norm_layer, **kwargs)
+class DANet(nn.Module):
+    def __init__(self, nclass, norm_layer=nn.BatchNorm2d):
+        super(DANet, self).__init__()
         self.num_classes = nclass
         self.pretrained = torchvision.models.segmentation.fcn_resnet50(pretrained=False, num_classes=self.num_classes)
-        self.head = DANetHead(2048, 2048, norm_layer)
-        # self.classifier = torchvision.models.segmentation.fcn.FCNHead(2048, self.num_classes)
-
-        # backbone = resnet.__dict__['resnet50'](
-        #     pretrained=False,
-        #     replace_stride_with_dilation=[False, True, True])
-        # return_layers = {'layer4': 'out'}
-        # self.backbone = torchvision.models._utils.IntermediateLayerGetter(backbone, return_layers=return_layers)
-        # self.classifier = torchvision.models.segmentation.fcn.FCNHead(2048, self.num_classes)
-        # base_model = torchvision.models.segmentation.fcn.FCN(backbone, classifier, None)
-        
+        self.head = DANetHead(2048, 2048, norm_layer)     
     
     def forward(self, x):
         input_shape = x.shape[-2:]
-        # contract: features is a dict of tensors
         features = self.pretrained.backbone(x)
-
         result = torchvision.models.segmentation._utils.OrderedDict()
         x = features["out"]
-
+        ## PAM & CAM
         x = self.head(x)[0]
-
         x = self.pretrained.classifier(x)
         x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
         result["out"] = x
-        # if self.aux_classifier is not None:
-        #     x = features["aux"]
-        #     x = self.aux_classifier(x)
-        #     x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
-        #     result["aux"] = x
         return result["out"]
-        # return self.pretrained(x)["out"]
-
-    # def forward(self, x):
-        # imsize = x.size()[2:]
-        # features = self.backbone(x)
-
-        # result = torchvision.models.segmentation._utils.OrderedDict()
-        # x = features["out"]
-        # x = self.classifier(x)
-        # x = F.interpolate(x, size=imsize, mode='bilinear', align_corners=False)
-        # result["out"] = x
-
-        # return result["out"]
-
-        # _, _, _, c4 = self.base_forward(x)
-        # x = self.base_forward(x)
-        # x = self.head(c4)
-        # x = list(x)
-        # x = self.classifier(x[0])
-        # x = self.classifier(x)
-        # x = F.interpolate(x, size=imsize, mode='bilinear', align_corners=False)
-        # return x
-
-        # x[0] = upsample(x[0], imsize, mode='bilinear', align_corners=True)
-        # x[1] = upsample(x[1], imsize, mode='bilinear', align_corners=True)
-        # x[2] = upsample(x[2], imsize, mode='bilinear', align_corners=True)
-
-        # outputs = [x[0]]
-        # outputs.append(x[1])
-        # outputs.append(x[2])
-        # return tuple(outputs)
-        # return x[0]
 
         
 class DANetHead(nn.Module):
