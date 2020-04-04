21b55392a49b62465540eb0e35a137d9c6ca38f4
lghfoo <1162820772@qq.com>
2020-04-04 10:04:12 +0800

    remove aux arg

--- DeepLearning/net/fcn/FCNResNet50.py
+++ DeepLearning/net/fcn/FCNResNet50.py
@@ -6,7 +6,7 @@ class FCNResNet50(nn.Module):
     def __init__(self, num_classes):
         super(FCNResNet50, self).__init__()
         self.num_classes = num_classes
-        self.pretrained = torchvision.models.segmentation.fcn_resnet50(pretrained=False, num_classes=self.num_classes, aux=False)
+        self.pretrained = torchvision.models.segmentation.fcn_resnet50(pretrained=False, num_classes=self.num_classes)
     
     def forward(self, x):
         return self.pretrained(x)["out"]
