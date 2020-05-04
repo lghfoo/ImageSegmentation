58fab365de049e4a44dd010daae87ebaebad6e0e
lghfoo <1162820772@qq.com>
2020-05-04 18:16:19 +0800

    fix arg

--- DeepLearning/net/deeplabv3/CustomDeepLabv3.py
+++ DeepLearning/net/deeplabv3/CustomDeepLabv3.py
@@ -34,10 +34,10 @@ def _segm_resnet(name, backbone_name, num_classes, aux, pretrained_backbone=True
     model = base_model(backbone, classifier, aux_classifier)
     return model
 
-def _load_model(arch_type, backbone, pretrained, progress, num_classes, aux_loss, **kwargs):
+def _load_model(arch_type, backbone, pretrained, progress, num_classes, aux_loss, dilation=[False, True, True], **kwargs):
     if pretrained:
         aux_loss = True
-    model = _segm_resnet(arch_type, backbone, num_classes, aux_loss, **kwargs)
+    model = _segm_resnet(arch_type, backbone, num_classes, aux_loss, dilation, **kwargs)
     if pretrained:
         arch = arch_type + '_' + backbone + '_coco'
         model_url = model_urls[arch]
@@ -49,7 +49,7 @@ def _load_model(arch_type, backbone, pretrained, progress, num_classes, aux_loss
     return model
 
 def deeplabv3_resnet50(pretrained=False, progress=True,
-                       num_classes=21, aux_loss=None, **kwargs):
+                       num_classes=21, aux_loss=None, dilation=[False, True, True], **kwargs):
     """Constructs a DeepLabV3 model with a ResNet-50 backbone.
 
     Args:
@@ -57,7 +57,18 @@ def deeplabv3_resnet50(pretrained=False, progress=True,
             contains the same classes as Pascal VOC
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _load_model('deeplabv3', 'resnet50', pretrained, progress, num_classes, aux_loss, **kwargs)
+    return _load_model('deeplabv3', 'resnet50', pretrained, progress, num_classes, aux_loss, dilation, **kwargs)
+
+def deeplabv3_resnet101(pretrained=False, progress=True,
+                       num_classes=21, aux_loss=None, dilation=[False, True, True], **kwargs):
+    """Constructs a DeepLabV3 model with a ResNet-101 backbone.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on COCO train2017 which
+            contains the same classes as Pascal VOC
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _load_model('deeplabv3', 'resnet101', pretrained, progress, num_classes, aux_loss, dilation, **kwargs)
 
 class CustomDeepLabv3(nn.Module):
     def __init__(self, num_classes,n_layers=50):
