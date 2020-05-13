5516e3f446963c213b18c6fed4fe4f02f156e81a
lghfoo <1162820772@qq.com>
2020-05-13 12:00:00 +0800

    update

--- DeepLearning/net/multimodulenet/MMNet.py
+++ DeepLearning/net/multimodulenet/MMNet.py
@@ -235,11 +235,11 @@ class ParallelMultiModule(nn.Module):
         # out = [x]
         out = []
         x = self.seq1(x)
-        for f in self.features:
-            out.append(F.interpolate(f(x), x_size[2:], mode='bilinear', align_corners=True))
-        # out.append(F.interpolate(self.features[0](x), x_size[2:], mode='bilinear', align_corners=True))
-        # out.append(F.interpolate(self.features[1](x), x_size[2:], mode='bilinear', align_corners=True))
-        # out.append(F.interpolate(self.features[2](x), x_size[2:], mode='bilinear', align_corners=True))
+        # for f in self.features:
+        #     out.append(F.interpolate(f(x), x_size[2:], mode='bilinear', align_corners=True))
+        out.append(F.interpolate(self.features[0](x), x_size[2:], mode='bilinear', align_corners=True))
+        out.append(F.interpolate(self.features[1](x), x_size[2:], mode='bilinear', align_corners=True))
+        out.append(F.interpolate(self.features[2](x), x_size[2:], mode='bilinear', align_corners=True))
         out = torch.cat(out, 1)
         out = self.project(out)
         return out
